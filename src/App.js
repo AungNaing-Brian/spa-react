7721be5348a88685f423edc6a5a9795353a73160
Aung Naing <aungnaing.brian@gmail.com>
2026-04-21 23:03:24 +0400

    c3ep13

--- src/App.js
+++ src/App.js
@@ -9,12 +9,34 @@ function App() {
     setName("Aung Naing");
     console.log(name);
   }
+
+  let [posts, setPosts] = useState([
+    {id: 1,
+     title: 'first post',
+    },
+    {
+      id: 2,
+      title: 'second post',
+    },
+    {
+      id: 3,
+      title: 'third post'
+    },
+  ]);
+  console.log(posts);
+
   return (
     <div className="App">
       <h1>Hello Friend</h1>
       <h2>This is {name}.</h2>
       <img src={Image} alt=''></img>
       <button onClick={changeName}>Change Name</button>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
