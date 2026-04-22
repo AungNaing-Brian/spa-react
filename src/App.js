96d6e3c686881745e745cb3a82ffc680735adc32
Aung Naing <aungnaing.brian@gmail.com>
2026-04-22 06:39:43 +0400

    c3ep16

--- src/App.js
+++ src/App.js
@@ -45,12 +45,15 @@ function App() {
       <h1>Posts</h1>
       <div className='list'>
         <ul>
-        {posts.map((post)=>(
+        {!!posts.length && posts.map((post)=>(
           <li key={post.id}>
             {post.title}
             <button onClick={()=>deletePost(post.id)}>delete</button>
             </li>
         ))}
+        {
+          !posts.length && <p>No Post Available</p>
+        }
       </ul>
       </div>
       {/* <div className='counterState'>
