f1e920c0846cd713a19380ae288c5368c3d5cc8e
Aung Naing <aungnaing.brian@gmail.com>
2026-04-25 23:55:46 +0400

    c3ep19p1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './components/Navbar/index';
 import Postslist from './components/Postslist';
+import Model from './components/Model'
 
 function App() {
   let [posts, setPosts] = useState([
@@ -22,6 +23,14 @@ function App() {
     <>
       <Navbar></Navbar>
       <Postslist post={posts}/>
+      <Model>
+        <h1>
+          Terms and Coditions
+        </h1>
+        <p>
+          contents .......
+        </p>
+      </Model>
     </>
   );
 }
