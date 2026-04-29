b8c753c73b0798bdd9cdfb092c00fb6033c95b96
Aung Naing <aungnaing.brian@gmail.com>
2026-04-29 23:25:21 +0400

    c4ep1

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,17 +1,15 @@
-import React, { Children } from 'react'
-import './index.css'
-import ReactDom from 'react-dom';
+import React, { Children } from "react";
+import "./index.css";
+import ReactDom from "react-dom";
 
-export default function modal({children}) {
-  return (
-    ReactDom.createPortal(
-       <div className='modal-backdrop'>
-        <div className='modal'>
-            {children}
-        </div>
-    </div> ,
-    document.getElementById('modal')
-    )
+export default function modal({ children }) {
+  return ReactDom.createPortal(
+    <div className="modal-component">
+      <div className="modal-backdrop">
+      <div className="modal">{children}</div>
+    </div>,
+    document.getElementById("modal"),
+    </div>
     
-  )
+  );
 }
--- src/components/Postslist/index.js
+++ src/components/Postslist/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import './index.css'
 export default function Postslist({post}) {
   return (
-    <div className='postlist'>
+    <div className='.postslist-component'>
+      <div className='postlist'>
         {post.map(posts=>(<div className='single-post' key={posts.id}>{posts.title}</div>))}
     </div>
+    </div>
+    
   )
 }
