7ad2bbc25d9860c290fb27380240ec78195c3fe3
Aung Naing <aungnaing.brian@gmail.com>
2026-05-02 23:45:37 +0400

    saveUpdate

--- src/components/Postslist/index.js
+++ src/components/Postslist/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './index.css'
+import styles from './single_post.module.css';
+
 export default function Postslist({post}) {
   return (
    
       <div className='postlist container'>
-        {post.map(posts=>(<div className='single-post' key={posts.id}>{posts.title}</div>))}
+        {post.map(posts=>(
+          <div className={`single-post ${styles.card}`} key={posts.id}>{posts.title}</div>
+          ))}
     </div>
     
     
