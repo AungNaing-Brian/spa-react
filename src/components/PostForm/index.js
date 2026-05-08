7bd6f8cac74fe2f5adcee65afef44b0ebb391c29
Aung Naing <aungnaing.brian@gmail.com>
2026-05-08 04:28:50 +0400

    c5ep28

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './index.css'
 import { useState } from 'react';
 
 export default function Index({addPost}) {
 
-    let [title, setTitle] = useState("")
+    // let [title, setTitle] = useState("")
+
+    let title = useRef();
     let resetForm = () => {
-        setTitle('');
+        // setTitle('');
+        title.current.value='';
         console.log("updated Successfully")
     }
     let upload_post = (e)=>{
         e.preventDefault();
         let post = {
             id : Math.floor(Math.random()*10000),
-            title : title
+            title : title.current.value
         }
         resetForm();
         addPost(post);
@@ -27,7 +30,8 @@ export default function Index({addPost}) {
         <label>
             Title
         </label>
-        <input type='text' onChange={(e)=>setTitle(e.target.value)} value={title}></input>
+        <input type='text' ref={title}></input>
+        {/* <input type='text' onChange={(e)=>setTitle(e.target.value)} value={title}></input> */}
     </div>
     
     <div className='form-control'>
